8c7c402b5694999146ec610992258c869107ffbd
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2024-03-04 18:48:24 +0800

    Update and rename ppo_train.py to ppo_training.py

--- ppo_training.py
+++ ppo_training.py
@@ -1,23 +1,23 @@
 # PPO: training
 # Dylan
-# 2024.3.2
+# 2024.3.4
 
 import gym
 import numpy as np
 import torch
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
 import time
 from ppo_agent import DDPGAgent
-import random
 
 # Initialize environment
-env = gym.make(id='Pendulum-v1')
+scenario = "Pendulum-v1"
+env = gym.make(id=scenario)
 STATE_DIM = env.observation_space.shape[0]
 ACTION_DIM = env.action_space.shape[0]
 
 # Hyperparameters
-NUM_EPISODE = 100
+NUM_EPISODE = 10
 NUM_STEP = 200
 
 # Initialize agent
@@ -42,33 +42,39 @@ for episode_i in range(NUM_EPISODE):
 
         agent.update()
         if done:
+            print("Done at step", step_i + 1)
             break
 
     REWARD_BUFFER[episode_i] = episode_reward
 
     print(f"Episode: {episode_i + 1}, Reward: {round(episode_reward, 2)}")
 
+env.close()
+
 current_path = os.path.dirname(os.path.realpath(__file__))
-model = current_path + '/models/'
+agent_path = current_path + "/models/" + scenario + '/'
 timestamp = time.strftime("%Y%m%d%H%M%S")
 
 # Save models
-actor_name = model + f'ddpg_actor_{timestamp}.pth'
-critic_name = model + f'ddpg_critic_{timestamp}.pth'
+flag = os.path.exists(agent_path)
+if not flag:
+    os.makedirs(agent_path)
+actor_name = agent_path + f'ppo_actor_{timestamp}.pth'
 torch.save(agent.actor.state_dict(), actor_name)
-torch.save(agent.critic.state_dict(), critic_name)
 
 # Close environment
-env.close()
+
 
 # Save the rewards as txt file
 np.savetxt(current_path + f'/ddpg_reward_{timestamp}.txt', REWARD_BUFFER)
 
-# # Plot rewards using ax.plot()
-# plt.plot(REWARD_BUFFER)
-# plt.xlabel('Episode')
-# plt.ylabel('Reward')
-# plt.title('DDPG Reward')
-# plt.grid()
-# plt.show()
-# plt.savefig(current_path + f'/ddpg_reward_{timestamp}.png')
+# Plot rewards using ax.plot()
+plt.plot(REWARD_BUFFER, color='purple', label='Episode Reward')
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.grid()
+plt.title(scenario)
+plt.legend()
+plt.savefig(f"reward_{timestamp}.png", format='png')
+plt.show()
+
