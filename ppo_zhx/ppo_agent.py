7557fe03b922d15ac995286179d1a702a57be91e
Hengxi Zhang <72500446+Dylan2020THU@users.noreply.github.com>
2024-03-17 22:27:03 +0800

    Update ppo_agent.py

--- ppo_zhx/ppo_agent.py
+++ ppo_zhx/ppo_agent.py
@@ -13,13 +13,6 @@ from torch.distributions import Normal
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device: ", device)
 
-LR_ACTOR = 1e-3
-LR_CRITIC = 1e-3
-GAMMA = 0.99
-TAU = 0.01
-LAMBDA = 0.95
-MEMORY_SIZE = 100000
-
 
 # Actor Network
 class Actor(nn.Module):
@@ -36,7 +29,7 @@ class Actor(nn.Module):
         x = torch.relu(self.fc2(x))
         mean = self.mean(x)
         log_std = self.log_std(x)
-        std = log_std.exp() # Ensure std is positive and allow different std for each action
+        std = log_std.exp()  # Ensure std is positive and allow different std for each action
         normal_dist = Normal(mean, std)
 
         z = normal_dist.rsample()  # For reparameterization trick
@@ -46,7 +39,7 @@ class Actor(nn.Module):
         action_log_prob = action_log_prob.sum(axis=-1, keepdim=True)  # Correction for the tanh squashing
 
         low, high = -2, 2
-        action = low + (action - low) * (high - low) / (high - low)  # Scale and shift to [-2,2]
+        action = low + 0.5 * (action + 1.0) * (high - low)  # Scale and shift to [-2,2]
         # action = action.clamp(low, high)  # Ensure action is within bounds
 
         return action, action_log_prob
@@ -69,62 +62,70 @@ class Critic(nn.Module):
 
 # Replay Buffer
 class ReplayMemory:
-    def __init__(self, capacity, state_dim, action_dim, batch_size):
-        self.capacity = capacity
-        self.batch_size = batch_size
-        self.state_cap = np.empty((self.capacity, state_dim), dtype=np.float32)
-        self.action_cap = np.empty((self.capacity, action_dim), dtype=np.float32)
-        self.log_prob_cap = np.empty((self.capacity, action_dim), dtype=np.float32)
-        self.value_cap = np.empty((self.capacity, 1), dtype=np.float32)
-        self.reward_cap = np.empty((self.capacity, 1), dtype=np.float32)
-        self.done_cap = np.empty((self.capacity, 1), dtype=bool)
-
-        self.count = 0  # To keep track of the actual number of experiences stored
-        self.current = 0  # To keep track of the current position for storing the next experience
+    def __init__(self, batch_size):
+        self.BATCH_SIZE = batch_size
+        self.state_cap = []
+        self.log_prob_cap = []
+        self.value_cap = []
+        self.action_cap = []
+        self.reward_cap = []
+        self.done_cap = []
 
     def add_memo(self, state, action, reward, log_prob, value, done):
-        self.state_cap[self.count] = state
-        self.action_cap[self.count] = action
-        self.log_prob_cap[self.count] = log_prob
-        self.value_cap[self.count] = value
-        self.reward_cap[self.count] = reward
-        self.done_cap[self.count] = done
-        self.count = min(self.count + 1, self.capacity)  # Ensure count does not exceed capacity
-        self.current += 1  # Move to the next position
+        self.state_cap.append(state)
+        self.action_cap.append(action)
+        self.reward_cap.append(reward)
+        self.log_prob_cap.append(log_prob)
+        self.value_cap.append(value)
+        self.done_cap.append(done)
 
     def sample(self):
-        # Ensure we can only sample if we have enough experiences
-        max_samples = min(self.count, self.batch_size)
-        idxes = np.random.choice(range(self.count), max_samples, replace=False)
+        num_state = len(self.state_cap)
+        # print(f"num_state: {num_state}")
+        batch_start_points = np.arange(0, num_state, self.BATCH_SIZE)
+        memory_indices = np.arange(num_state, dtype=np.int32)
+        np.random.shuffle(memory_indices)
+        batches = [memory_indices[i:i + self.BATCH_SIZE] for i in batch_start_points]
 
-        states = self.state_cap[idxes]
-        actions = self.action_cap[idxes]
-        log_probs = self.log_prob_cap[idxes]
-        values = self.value_cap[idxes]
-        rewards = self.reward_cap[idxes]
-        dones = self.done_cap[idxes]
-
-        return states, actions, log_probs, values, rewards, dones
+        return np.array(self.state_cap), \
+            np.array(self.action_cap), \
+            np.array(self.reward_cap), \
+            np.array(self.log_prob_cap), \
+            np.array(self.value_cap), \
+            np.array(self.done_cap), \
+            batches
 
     def clear_memo(self):
-        self.count = 0
-        self.current = 0
+        self.state_cap = []
+        self.action_cap = []
+        self.reward_cap = []
+        self.log_prob_cap = []
+        self.value_cap = []
+        self.done_cap = []
 
 
 # PPO Agent
 class PPOAgent:
-    def __init__(self, state_dim, action_dim, batch_size):
+    def __init__(self, state_dim, action_dim):
 
-        self.epsilon_clip = 0.2
-        self.batch_size = batch_size
+        self.LR_ACTOR = 1e-3
+        self.LR_CRITIC = 1e-3
+        self.GAMMA = 0.99
+        self.LAMBDA = 0.95
+        self.TAU = 0.005
+        self.EPSILON_CLIP = 0.2
+        self.EPOCH = 10
+        self.BATCH_SIZE = 5
 
         self.actor = Actor(state_dim, action_dim).to(device)
-        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=LR_ACTOR)
+        self.target_actor = Actor(state_dim, action_dim).to(device)
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=self.LR_ACTOR)
 
         self.critic = Critic(state_dim, action_dim).to(device)
-        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=LR_CRITIC)
+        self.target_critic = Critic(state_dim, action_dim).to(device)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=self.LR_CRITIC)
 
-        self.replay_buffer = ReplayMemory(MEMORY_SIZE, state_dim, action_dim, self.batch_size)
+        self.replay_buffer = ReplayMemory(self.BATCH_SIZE)
 
     def get_action(self, state):
         state = torch.FloatTensor(state).unsqueeze(0).to(device)
@@ -136,54 +137,67 @@ class PPOAgent:
 
     def update(self):
 
-        states, actions, rewards, log_probs, values, dones = self.replay_buffer.sample()
-        states = torch.FloatTensor(states).to(device)
-        rewards = torch.FloatTensor(rewards).unsqueeze(1).to(device)
-        log_probs = torch.FloatTensor(log_probs).unsqueeze(1).to(device)
-        values = torch.FloatTensor(values).to(device)
-        dones = torch.FloatTensor(dones).unsqueeze(1).to(device)
-        # print(f"rewards: {rewards}")
+        for epoch_i in range(self.EPOCH):
+            memo_states, memo_actions, memo_rewards, memo_action_log_probs, memo_values, memo_dones, \
+                memo_batches = self.replay_buffer.sample()
+            # print(f"rewards: {rewards}")
 
-        advantages = torch.empty(len(rewards), dtype=torch.float32)
-        for t in range(len(rewards) - 1):
-            discount = 1
-            a_t = 0
-            for k in range(t, len(rewards) - 1):
-                a_t += discount * (rewards[k] + GAMMA * values[k + 1] * (1 - int(dones[k])) - values[k])
-                discount *= GAMMA * LAMBDA
-            advantages[t] = a_t
-        advantages = torch.FloatTensor(advantages).unsqueeze(1).to(device)
+            memo_values = memo_values.squeeze(1)
+            advantages = np.empty(len(memo_rewards), dtype=np.float32)
+            for t in range(len(memo_rewards) - 1):
+                discount = 1
+                a_t = 0
+                for k in range(t, len(memo_rewards) - 1):
+                    a_t += discount * (memo_rewards[k] + self.GAMMA * memo_values[k + 1] * (1 - int(memo_dones[k])) - memo_values[k])
+                    discount *= self.GAMMA * self.LAMBDA
+                advantages[t] = a_t
 
-        # Actor update using PPO clip
-        next_log_probs = self.actor.forward(states)[1]
-        ratio = torch.exp(next_log_probs - log_probs)
-        surr1 = ratio * advantages
-        surr2 = torch.clamp(ratio, 1 - self.epsilon_clip, 1 + self.epsilon_clip) * advantages
-        actor_loss = -torch.min(surr1, surr2).mean()
-        print(f"actor loss: {round(actor_loss,2)}")
+            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+            advantages_tensor = torch.tensor(advantages, dtype=torch.float32).to(device)
+            memo_values_tensor = torch.tensor(memo_values, dtype=torch.float32).to(device)
 
+            # N actors work simultaneously
+            for batch_i in memo_batches:
+                memo_batch_states_tensor = torch.tensor(memo_states[batch_i]).to(device)
+                memo_batch_action_log_probs_tensor = torch.tensor(memo_action_log_probs[batch_i]).to(device)
+                # memo_actions_tensor = torch.tensor(memo_actions[batch_i]).to(device)
 
-        # Update critic with returns
-        returns = advantages + values
-        critic_values = self.critic.forward(states)
-        critic_loss = F.mse_loss(critic_values, returns)
-        print(f"critic loss: {round(critic_loss,2)}")
+                # Actor loss
+                next_action_log_probs_tensor = self.actor.forward(memo_batch_states_tensor)[1]
+                ratio = torch.exp(next_action_log_probs_tensor - memo_batch_action_log_probs_tensor)
+                surr1 = ratio * advantages_tensor[batch_i]
+                surr2 = torch.clamp(ratio, 1 - self.EPSILON_CLIP, 1 + self.EPSILON_CLIP) * advantages_tensor[batch_i]
+                actor_loss = -torch.min(surr1, surr2).mean()
+                # print(f"actor loss: {actor_loss}")
 
-        # Method 1: separate actor and critic loss
-        self.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        self.actor_optimizer.step()
-        self.critic_optimizer.zero_grad()  # zero the gradients
-        critic_loss.backward()
-        self.critic_optimizer.step()  # update the weights
+                # Critic loss
+                returns = advantages_tensor[batch_i] + memo_values_tensor[batch_i]
+                critic_values = self.critic.forward(memo_batch_states_tensor).squeeze(1)
+                critic_loss = nn.MSELoss()(critic_values, returns)
+                # print(f"critic loss: {critic_loss}")
 
-        # Method 2: combined total loss
-        # total_loss = actor_loss + 0.1 *critic_loss
-        # self.actor_optimizer.zero_grad()
-        # self.critic_optimizer.zero_grad()
-        # total_loss.backward()
-        # self.actor_optimizer.step()
-        # self.critic_optimizer.step()
+                # Method 1: separate actor and critic loss
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+                self.critic_optimizer.zero_grad()  # zero the gradients
+                critic_loss.backward()
+                self.critic_optimizer.step()  # update the weights
 
+                # # Method 2: combined total loss
+                # total_loss = actor_loss + 0.2 *critic_loss
+                # self.actor_optimizer.zero_grad()
+                # self.critic_optimizer.zero_grad()
+                # total_loss.backward()
+                # self.actor_optimizer.step()
+                # self.critic_optimizer.step()
 
-        # self.replay_buffer.clear_memo()  # Clear the replay buffer after updating the networks
+                # # Update target critic
+                # for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
+                #     target_param.data.copy_(self.TAU * param.data + (1.0 - self.TAU) * target_param.data)
+                #
+                # # Update target actor
+                # for target_param, param in zip(self.target_actor.parameters(), self.actor.parameters()):
+                #     target_param.data.copy_(self.TAU * param.data + (1.0 - self.TAU) * target_param.data)
+
+        self.replay_buffer.clear_memo()  # Clear the replay buffer after updating the networks
